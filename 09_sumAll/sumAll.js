413d661300c767a6cdb344b4fd91a3bbccb915c7
Aditya Gayan <adityagayan1303@gmail.com>
2025-12-29 20:10:07 +0000

    exercises from 6 till 11 are done

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if(!Number.isInteger(a) || !Number.isInteger(b)){
+        return 'ERROR';
+    }
+    if(a < 0 || b < 0){
+        return 'ERROR';
+    }
+    let mx = Math.max(a, b);
+    let mn = Math.min(a, b);
+    let sum = 0;
+    for(let i = mn; i <= mx; i++){
+        sum = sum + i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
